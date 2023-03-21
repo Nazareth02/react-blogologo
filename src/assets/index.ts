dd79e9c8c037f3676b3e476a81552ee8aaeb77fd
Nazareth <nazares2002@gmail.com>
2023-03-21 23:48:12 +0300

    feat(assetss): add all icons to the project

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,4 +1,6 @@
 import { ReactComponent as Logo } from "./img/logo.svg";
 import { ReactComponent as FavoritesActiveIcon } from "./icons/favorite.svg";
+import { ReactComponent as SingInIcon } from "./icons/sign-in.svg";
+import { ReactComponent as SingUpIcon } from "./icons/sign-up.svg";
 
-export { Logo, FavoritesActiveIcon };
+export { Logo, FavoritesActiveIcon, SingInIcon, SingUpIcon };
